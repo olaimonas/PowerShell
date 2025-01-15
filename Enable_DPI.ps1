b3a4e60b3986a16837d7792f337abfb94be4ae23
olaimonas <olaimonas@gmail.com>
2025-01-15 20:20:56 +0200

    Add files via upload

---
+++ Enable_DPI.ps1
@@ -0,0 +1,41 @@
+﻿# Change the value of Enable_DPI to a desired value in the Windows Registry
+# The modification of the 'Enable_DPI' value requires administrator rights
+
+$ErrorActionPreference = 'silentlycontinue'
+cd "$PSSCriptRoot"
+$path = "HKLM:\SOFTWARE\Policies\Citrix\ICA Client\DPI"
+$regKeyName = "Enable_DPI"
+
+Function getEnable_DPIValue {
+    $regKeyValue = $null
+    $parameters = @{
+        ScriptBlock = {
+            Param ($path, $regKeyName) 
+            Get-ItemProperty -Path $path -Name $regKeyName | Select-Object -ExpandProperty $regKeyName
+        }
+        ArgumentList = $path, $regKeyName
+    }
+    Invoke-Command @parameters
+}
+
+Function setEnable_DPIValue() {
+    $Choice = Read-Host "Enter the desired value"
+    Set-ItemProperty -Path $path -Name $regKeyName -Value $Choice
+}
+
+Function executeScript {
+
+    $Enable_DPIValue = getEnable_DPIValue
+    
+    $yesNo = (Read-Host "'Enable_DPI' value is $Enable_DPIValue. Do you want to modify it? [Y/N]?").ToUpper().Trim();
+        if($yesNo -eq "Y") {
+            setEnable_DPIValue;
+            $Enable_DPIValue = getEnable_DPIValue
+            Write-Host "'Enable_DPI' value is now $Enable_DPIValue"
+        } else {
+            $Enable_DPIValue = getEnable_DPIValue
+            Write-Host "'Enable_DPI' value has not been modified and is equal to $Enable_DPIValue"
+            }
+}
+
+executeScript
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Place the shortcut file "Enable_DPI.bat" on the Desktop or other convenient place of your choice.
+The shortcut points to "Enable_DPI.bat" Windows Batch File which points to "Enable_DPI.ps1" Windows PowerShell Script file.
+The files "Enable_DPI.ps1" and "Enable_DPI.bat" must be in the same folder.
+Use the script to set the value to "2" to have the resolution setting of "Let the operating system scale the resolution" in Citrix.
